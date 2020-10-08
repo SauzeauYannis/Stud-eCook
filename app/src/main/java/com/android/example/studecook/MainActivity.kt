d054f125072752178f653a975b4c58bd9d8127a2
Yannis <sauzeauyannis@gmail.com>
2020-10-08 22:53:49 +0200

    anonymous, tel, mail and google creation accounts work

--- app/src/main/java/com/android/example/studecook/MainActivity.kt
+++ app/src/main/java/com/android/example/studecook/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.android.example.studecook
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,13 +18,14 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
 
         navView.setupWithNavController(navController)
-        FirebaseAuth.AuthStateListener { firebaseAuth ->
-            if(firebaseAuth.currentUser != null) {
-                AuthUI.getInstance()
-                    .signOut(this)
-            } else {
-                createSignInIntent()
-            }
+        val user = FirebaseAuth.getInstance().currentUser
+        if(user != null) {
+            AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                }
+        } else {
+            createSignInIntent()
         }
     }
 
@@ -51,7 +48,7 @@ class MainActivity : AppCompatActivity() {
                 .build(),
             RC_SIGN_IN)
     }
-
+/*
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -63,7 +60,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
+*/
     companion object {
 
         private const val RC_SIGN_IN = 123
