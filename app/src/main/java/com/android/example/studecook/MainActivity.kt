342313a0eb9e3fa84a2e158f9d0cbba0f90bf0c2
Yannis <sauzeauyannis@gmail.com>
2020-10-08 18:47:29 +0200

    add account creation

--- app/src/main/java/com/android/example/studecook/MainActivity.kt
+++ app/src/main/java/com/android/example/studecook/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.android.example.studecook
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +21,46 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
 
         navView.setupWithNavController(navController)
+        createSignInIntent()
+    }
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            //AuthUI.IdpConfig.FacebookBuilder().build(),
+            //AuthUI.IdpConfig.TwitterBuilder().build(),
+            AuthUI.IdpConfig.AppleBuilder().build(),
+            AuthUI.IdpConfig.AnonymousBuilder().build(),
+            AuthUI.IdpConfig.GitHubBuilder().build(),
+            AuthUI.IdpConfig.MicrosoftBuilder().build(),
+            AuthUI.IdpConfig.YahooBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+            } else {
+            }
+        }
+    }
+
+    companion object {
+
+        private const val RC_SIGN_IN = 123
     }
 }
