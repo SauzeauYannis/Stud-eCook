74ab1c78c4b254f01148ec2e991cf0be10adb8ab
Yannis <sauzeauyannis@gmail.com>
2020-12-28 16:25:30 +0100

    Save fragment_add_step1 datas

--- app/src/main/java/com/android/app/studecook/ui/add/AddStep1Fragment.kt
+++ app/src/main/java/com/android/app/studecook/ui/add/AddStep1Fragment.kt
@@ -1,5 +1,6 @@
 package com.android.app.studecook.ui.add
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -35,11 +36,13 @@ class AddStep1Fragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_add_step1, container, false)
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
 
         root.text_add_title.text = getString(R.string.text_add_title, "1")
+        root.text_input_add_name.setText(sharedPref?.getString(getString(R.string.saved_add_name_key), null))
 
-        textInputName = root.text_input_add_name
-        root.text_input_add_name.editText?.setOnClickListener {
+        textInputName = root.text_input_layout_add_name
+        root.text_input_layout_add_name.editText?.setOnClickListener {
             textInputName.error = null
         }
 
@@ -50,6 +53,9 @@ class AddStep1Fragment : Fragment() {
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             root.array_add_time.adapter = adapter
+            root.array_add_time.setSelection(
+                sharedPref!!.getInt(getString(R.string.saved_add_time_key), 0)
+            )
         }
 
         ArrayAdapter.createFromResource(
@@ -59,6 +65,9 @@ class AddStep1Fragment : Fragment() {
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             root.array_add_price.adapter = adapter
+            root.array_add_price.setSelection(
+                sharedPref!!.getInt(getString(R.string.saved_add_price_key), 0)
+            )
         }
 
         root.button_add_next.setOnClickListener {
@@ -66,6 +75,12 @@ class AddStep1Fragment : Fragment() {
                 findNavController().navigate(R.id.action_navigation_add_step1_to_navigation_add_step2)
             } else {
                 Toast.makeText(root.context, getString(R.string.add_name_false), Toast.LENGTH_SHORT).show()
+            }
+            with (sharedPref!!.edit()) {
+                putString(getString(R.string.saved_add_name_key), root.text_input_add_name.text.toString())
+                putInt(getString(R.string.saved_add_time_key), root.array_add_time.selectedItemPosition)
+                putInt(getString(R.string.saved_add_price_key), root.array_add_price.selectedItemPosition)
+                apply()
             }
         }
 
