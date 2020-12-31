41be75c3d8d625cda3b46abf5d337a331c6f5e84
Yannis <sauzeauyannis@gmail.com>
2020-12-31 12:24:51 +0100

    Fixes connection problem due to an old firebaseUi auth version

--- app/src/main/java/com/android/app/studecook/MainActivity.kt
+++ app/src/main/java/com/android/app/studecook/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.android.app.studecook
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 open class MainActivity : AppCompatActivity() {
 
     companion object {
+        const val TAG = "MainFragment"
         const val RC_SIGN_IN = 123
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val sharedPreferences = getSharedPreferences("dark", 0)
-        val isDark = sharedPreferences.getBoolean("dark_mode", true)
+        val isDark = sharedPreferences.getBoolean("dark_mode", false)
         if (isDark) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         } else {
@@ -30,8 +36,7 @@ open class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
 
         nav_view.setupWithNavController(navController)
-        val user = FirebaseAuth.getInstance().currentUser
-        if(user == null) {
+        if(FirebaseAuth.getInstance().currentUser == null) {
             createSignInIntent()
         }
     }
@@ -40,7 +45,7 @@ open class MainActivity : AppCompatActivity() {
         val providers = arrayListOf(
             AuthUI.IdpConfig.AnonymousBuilder().build(),
             AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().setDefaultCountryIso("fr").build(),
             AuthUI.IdpConfig.GoogleBuilder().build(),
             AuthUI.IdpConfig.FacebookBuilder().build(),
             AuthUI.IdpConfig.TwitterBuilder().build()
@@ -51,7 +56,27 @@ open class MainActivity : AppCompatActivity() {
                 .createSignInIntentBuilder()
                 .setAvailableProviders(providers)
                 .setTheme(R.style.AppTheme)
+                .enableAnonymousUsersAutoUpgrade()
                 .build(),
             RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // User successfully signed in
+                Log.i(
+                    TAG,
+                    "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                )
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
     }
 }
--- app/src/main/java/com/android/app/studecook/ui/account/AccountViewModel.kt
+++ app/src/main/java/com/android/app/studecook/ui/account/AccountViewModel.kt
@@ -10,9 +10,20 @@ import com.google.firebase.auth.FirebaseAuth
 class AccountViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _text = MutableLiveData<String>().apply {
-        value = application.getString(R.string.text_account,
-            FirebaseAuth.getInstance().currentUser?.displayName
-        )
+        val user = FirebaseAuth.getInstance().currentUser
+        value = if (user == null) {
+            application.getString(R.string.text_no_account)
+        } else {
+            if (user.displayName != null) {
+                application.getString(R.string.text_account,
+                    FirebaseAuth.getInstance().currentUser?.displayName
+                )
+            } else {
+                application.getString(R.string.text_account,
+                    application.getString(R.string.anonymous)
+                )
+            }
+        }
     }
     val text: LiveData<String> = _text
 }
