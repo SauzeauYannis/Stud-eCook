a5209d7ab2a01e0a7e206cd3477bd2ed1476a54d
Yannis <sauzeauyannis@gmail.com>
2021-02-13 11:20:54 +0100

    Add image on RecipeFragment.kt and adjust size on CardView

--- app/src/main/java/com/android/app/studecook/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/android/app/studecook/ui/recipe/RecipeFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.android.app.studecook.R
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.fragment_recipe.view.*
 
 class RecipeFragment : Fragment() {
@@ -35,8 +38,17 @@ class RecipeFragment : Fragment() {
             findNavController().navigate(R.id.action_recipeFragment_to_navigation_home)
         }
 
-        root.text_recipe_name.text = "name : ${args.currentRecipe.name}\n\n" +
-                "type : " + resources.getStringArray(R.array.type_array)[args.currentRecipe.type!!]
+        root.text_recipe_name.text = args.currentRecipe.name
+
+        if (args.currentRecipe.image != null) {
+            val storageReference = FirebaseStorage.getInstance().reference.child(args.currentRecipe.image!!)
+            storageReference.downloadUrl.addOnSuccessListener { uri ->
+                Glide.with(requireContext())
+                    .load(uri)
+                    .into(root.image_recipe)
+            }
+        } else
+            root.image_recipe.visibility = ImageView.INVISIBLE
 
         return root
     }
