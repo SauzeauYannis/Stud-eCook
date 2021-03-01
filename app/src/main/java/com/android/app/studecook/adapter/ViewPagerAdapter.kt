67ee27eb1c256f84ebf9d9af398af7e17860b257
Yannis <sauzeauyannis@gmail.com>
2021-03-01 19:01:43 +0100

    Fix query and swipe bugs in sub and fav pages

--- app/src/main/java/com/android/app/studecook/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/android/app/studecook/adapter/ViewPagerAdapter.kt
@@ -48,112 +48,8 @@ class ViewPagerAdapter : RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>
 
     override fun onBindViewHolder(holder: Pager2ViewHolder, position: Int) {
         setUpRecyclerView(holder, position)
-
-
-
-        holder.itemFabFilter.setOnClickListener {
-            val dialog = Dialog(holder.itemFabFilter.context)
-            dialog.setCancelable(true)
-            dialog.setContentView(R.layout.dialog_filter)
-            generateCheckButtons(dialog)
-            dialog.findViewById<ImageView>(R.id.image_hide_filter).setOnClickListener {
-                dialog.hide()
-            }
-            dialog.findViewById<Button>(R.id.filter_button).setOnClickListener{
-                val query = generateQuery(dialog, holder)
-                val firestoreRecyclerOptions = FirestoreRecyclerOptions.Builder<RecipeModel>()
-                        .setQuery(query, RecipeModel::class.java)
-                        .build()
-                recipeAdapter!!.updateOptions(firestoreRecyclerOptions)
-                dialog.hide()
-            }
-            dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-            dialog.show()
-
-        }
-
-        holder.itemSwipe.setOnRefreshListener {
-            val query = collectionReference.orderBy("date", Query.Direction.DESCENDING)
-            val firestoreRecyclerOptions = FirestoreRecyclerOptions.Builder<RecipeModel>()
-                    .setQuery(query, RecipeModel::class.java)
-                    .build()
-            recipeAdapter!!.updateOptions(firestoreRecyclerOptions)
-            Toast.makeText(holder.itemView.context, holder.itemView.context.getString(R.string.toast_up_to_date), Toast.LENGTH_SHORT).show()
-            holder.itemSwipe.isRefreshing = false
-        }
     }
 
-    private fun generateCheckButtons(dialog: Dialog) {
-        val checkPrim: SwitchMaterial = dialog.findViewById(R.id.switchPrimary)
-        val radioGroupPrim: RadioGroup = dialog.findViewById(R.id.filter_radioGroup_primary)
-
-        val checkCat: SwitchMaterial = dialog.findViewById(R.id.switchCategory)
-        val radioGroupCat: RadioGroup = dialog.findViewById(R.id.filter_radioGroup_category)
-
-        val checkDiet: SwitchMaterial = dialog.findViewById(R.id.switchDiet)
-        val radioGroupDiet: RadioGroup = dialog.findViewById(R.id.filter_radioGroup_diet)
-
-        setEnableRadioGroup(checkPrim, radioGroupPrim)
-        setEnableRadioGroup(checkCat, radioGroupCat)
-        setEnableRadioGroup(checkDiet, radioGroupDiet)
-    }
-
-    private fun generateQuery(dialog: Dialog, holder: Pager2ViewHolder): Query {
-
-        val checkPrim: SwitchMaterial = dialog.findViewById(R.id.switchPrimary)
-        val checkCat: SwitchMaterial = dialog.findViewById(R.id.switchCategory)
-        val checkDiet: SwitchMaterial = dialog.findViewById(R.id.switchDiet)
-
-        var query: Query = collectionReference
-
-        val filterPrimaries = dialog.findViewById<RadioGroup>(R.id.filter_radioGroup_primary)
-
-        if(checkPrim.isChecked){
-            for (i in 1 until filterPrimaries.childCount) {
-                val checkBox: CheckBox = filterPrimaries.getChildAt(i) as CheckBox
-                if(checkBox.isChecked){
-                    query = when (i) {
-                        1 -> query.orderBy("fav", Query.Direction.DESCENDING)
-                        2 -> query.orderBy("price", Query.Direction.ASCENDING)
-                        else -> query.orderBy("time", Query.Direction.ASCENDING)
-                    }
-                }
-            }
-        }
-
-        val filterCategory = dialog.findViewById<RadioGroup>(R.id.filter_radioGroup_category)
-        val radioCheckedFilterCategory = filterCategory.checkedRadioButtonId
-
-        if(checkCat.isChecked) {
-            when (filterCategory.indexOfChild(filterCategory.findViewById(radioCheckedFilterCategory))) {
-                1 -> query = query.whereEqualTo("type", 0)
-                2 -> query = query.whereEqualTo("type", 1)
-                3 -> query = query.whereEqualTo("type", 2)
-                else -> query = query.whereEqualTo("type", 3)
-            }
-        }
-
-        val filterRegime = dialog.findViewById<RadioGroup>(R.id.filter_radioGroup_diet)
-        val radioCheckedFilterRegime = filterRegime.checkedRadioButtonId
-
-        if(checkDiet.isChecked) {
-            when (filterRegime.indexOfChild(filterRegime.findViewById(radioCheckedFilterRegime))) {
-                1 -> query = query.whereEqualTo("diet", 0)
-                2 -> query = query.whereEqualTo("diet", 1)
-                else -> query = query.whereEqualTo("diet", 2)
-            }
-        }
-
-        return query
-    }
-
-    private fun setEnableRadioGroup(checkPrim:SwitchMaterial ,group: RadioGroup){
-        checkPrim.setOnClickListener{
-            for (i in 0 until group.childCount) {
-                group.getChildAt(i).isEnabled = checkPrim.isChecked
-            }
-        }
-    }
     private fun setUpRecyclerView(holder: Pager2ViewHolder, position: Int) {
         holder.itemSwipe.isRefreshing = true
 
@@ -191,6 +87,12 @@ class ViewPagerAdapter : RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>
                                     .setQuery(query, RecipeModel::class.java)
                                     .build()
 
+                            setSwipe(holder, firestoreRecyclerOptions)
+
+                            holder.itemFabFilter.setOnClickListener {
+                                generateFilterDialog(holder, query)
+                            }
+
                             recipeAdapter = HomeRecipeAdapter(firestoreRecyclerOptions)
                             holder.itemRecycler.layoutManager = GridLayoutManager(holder.itemRecycler.context, 2, LinearLayoutManager.VERTICAL, false)
                             holder.itemRecycler.adapter = recipeAdapter
@@ -205,6 +107,12 @@ class ViewPagerAdapter : RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>
         val firestoreRecyclerOptions = FirestoreRecyclerOptions.Builder<RecipeModel>()
                 .setQuery(query, RecipeModel::class.java)
                 .build()
+
+        setSwipe(holder, firestoreRecyclerOptions)
+
+        holder.itemFabFilter.setOnClickListener {
+            generateFilterDialog(holder, query)
+        }
 
         recipeAdapter = HomeRecipeAdapter(firestoreRecyclerOptions)
         holder.itemRecycler.layoutManager = GridLayoutManager(holder.itemRecycler.context, 2, LinearLayoutManager.VERTICAL, false)
@@ -231,12 +139,120 @@ class ViewPagerAdapter : RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>
                                     .setQuery(query, RecipeModel::class.java)
                                     .build()
 
+                            holder.itemFabFilter.setOnClickListener {
+                                generateFilterDialog(holder, query)
+                            }
+
+                            setSwipe(holder, firestoreRecyclerOptions)
+
                             recipeAdapter = HomeFavRecipeAdapter(firestoreRecyclerOptions)
                             holder.itemRecycler.layoutManager = GridLayoutManager(holder.itemRecycler.context, 2, LinearLayoutManager.VERTICAL, false)
                             holder.itemRecycler.adapter = recipeAdapter
                             recipeAdapter!!.startListening()
                         }
                     }
+        }
+    }
+
+    private fun generateFilterDialog(holder: Pager2ViewHolder, query: Query) {
+        val dialog = Dialog(holder.itemFabFilter.context)
+        dialog.setCancelable(true)
+        dialog.setContentView(R.layout.dialog_filter)
+        generateCheckButtons(dialog)
+        dialog.findViewById<ImageView>(R.id.image_hide_filter).setOnClickListener {
+            dialog.hide()
+        }
+        dialog.findViewById<Button>(R.id.filter_button).setOnClickListener {
+            val generatedQuery = generateQuery(dialog, query)
+            val firestoreRecyclerOptions = FirestoreRecyclerOptions.Builder<RecipeModel>()
+                    .setQuery(generatedQuery, RecipeModel::class.java)
+                    .build()
+            recipeAdapter!!.updateOptions(firestoreRecyclerOptions)
+            dialog.hide()
+        }
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.show()
+    }
+
+    private fun generateCheckButtons(dialog: Dialog) {
+        val checkPrim: SwitchMaterial = dialog.findViewById(R.id.switchPrimary)
+        val radioGroupPrim: RadioGroup = dialog.findViewById(R.id.filter_radioGroup_primary)
+
+        val checkCat: SwitchMaterial = dialog.findViewById(R.id.switchCategory)
+        val radioGroupCat: RadioGroup = dialog.findViewById(R.id.filter_radioGroup_category)
+
+        val checkDiet: SwitchMaterial = dialog.findViewById(R.id.switchDiet)
+        val radioGroupDiet: RadioGroup = dialog.findViewById(R.id.filter_radioGroup_diet)
+
+        setEnableRadioGroup(checkPrim, radioGroupPrim)
+        setEnableRadioGroup(checkCat, radioGroupCat)
+        setEnableRadioGroup(checkDiet, radioGroupDiet)
+    }
+
+    private fun generateQuery(dialog: Dialog, query: Query): Query {
+        var newQuery = query
+
+        val checkPrim: SwitchMaterial = dialog.findViewById(R.id.switchPrimary)
+        val checkCat: SwitchMaterial = dialog.findViewById(R.id.switchCategory)
+        val checkDiet: SwitchMaterial = dialog.findViewById(R.id.switchDiet)
+
+        val filterPrimaries = dialog.findViewById<RadioGroup>(R.id.filter_radioGroup_primary)
+
+        if(checkPrim.isChecked){
+            for (i in 1 until filterPrimaries.childCount) {
+                val checkBox: CheckBox = filterPrimaries.getChildAt(i) as CheckBox
+                if(checkBox.isChecked){
+                    newQuery = when (i) {
+                        1 -> newQuery.orderBy("fav", Query.Direction.DESCENDING)
+                        2 -> newQuery.orderBy("price", Query.Direction.ASCENDING)
+                        else -> newQuery.orderBy("time", Query.Direction.ASCENDING)
+                    }
+                }
+            }
+        }
+
+        val filterCategory = dialog.findViewById<RadioGroup>(R.id.filter_radioGroup_category)
+        val radioCheckedFilterCategory = filterCategory.checkedRadioButtonId
+
+        if(checkCat.isChecked) {
+            newQuery = when (filterCategory.indexOfChild(filterCategory.findViewById(radioCheckedFilterCategory))) {
+                1 -> newQuery.whereEqualTo("type", 0)
+                2 -> newQuery.whereEqualTo("type", 1)
+                3 -> newQuery.whereEqualTo("type", 2)
+                else -> newQuery.whereEqualTo("type", 3)
+            }
+        }
+
+        val filterRegime = dialog.findViewById<RadioGroup>(R.id.filter_radioGroup_diet)
+        val radioCheckedFilterRegime = filterRegime.checkedRadioButtonId
+
+        if(checkDiet.isChecked) {
+            newQuery = when (filterRegime.indexOfChild(filterRegime.findViewById(radioCheckedFilterRegime))) {
+                1 -> newQuery.whereEqualTo("diet", 0)
+                2 -> newQuery.whereEqualTo("diet", 1)
+                else -> newQuery.whereEqualTo("diet", 2)
+            }
+        }
+
+        return newQuery
+    }
+
+    private fun setEnableRadioGroup(checkPrim:SwitchMaterial ,group: RadioGroup){
+        checkPrim.setOnClickListener{
+            for (i in 0 until group.childCount) {
+                group.getChildAt(i).isEnabled = checkPrim.isChecked
+            }
+        }
+    }
+
+    private fun setSwipe(
+            holder: Pager2ViewHolder,
+            firestoreRecyclerOptions: FirestoreRecyclerOptions<RecipeModel>
+    ) {
+        holder.itemSwipe.setOnRefreshListener {
+            recipeAdapter!!.updateOptions(firestoreRecyclerOptions)
+            Toast.makeText(holder.itemView.context, holder.itemView.context.getString(R.string.toast_up_to_date), Toast.LENGTH_SHORT).show()
+            holder.itemSwipe.isRefreshing = false
         }
     }
 
